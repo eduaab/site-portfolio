1c6a5476c9fdb0f8b211d6f0ef69f76c822ddeca
eduaab <eduardoaa1901@gmail.com>
2024-11-27 00:30:53 -0300

    Atualização do site

--- js/arquivo.js
+++ js/arquivo.js
@@ -2,7 +2,7 @@ document.querySelector('.hero-link').addEventListener('click', function (event) 
     event.preventDefault(); // Impede o comportamento padrão do link
   
     // Faz o fetch do conteúdo da nova página
-    fetch('sobre.html')
+    fetch('projetos.html')
       .then(response => {
         if (!response.ok) {
           throw new Error(`HTTP status ${response.status}`);
@@ -19,3 +19,26 @@ document.querySelector('.hero-link').addEventListener('click', function (event) 
       });
   });
   
+
+  document.querySelector('.hero-link').addEventListener('click', function (event) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+  
+    // Faz o fetch do conteúdo da nova página
+    fetch('int-exp.html')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP status ${response.status}`);
+        }
+        return response.text();
+      })
+      .then(data => {
+        // Substitui todo o conteúdo da página
+        document.body.innerHTML = data;
+      })
+      .catch(error => {
+        console.error('Erro ao carregar o conteúdo:', error);
+        alert('Erro ao carregar a página. Tente novamente mais tarde.');
+      });
+  });
+  
+
