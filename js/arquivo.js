865ae1ac5afb1b00b8f5982c8d4ba12f761837ac
eduaab <eduardoaa1901@gmail.com>
2024-11-26 22:10:09 -0300

    Primeiro commit do desenvolvimento do site

---
+++ js/arquivo.js
@@ -0,0 +1,21 @@
+document.querySelector('.hero-link').addEventListener('click', function (event) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+  
+    // Faz o fetch do conteúdo da nova página
+    fetch('sobre.html')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP status ${response.status}`);
+        }
+        return response.text();
+      })
+      .then(data => {
+        // Substitui todo o conteúdo da página
+        document.body.innerHTML = data;
+      })
+      .catch(error => {
+        console.error('Erro ao carregar o conteúdo:', error);
+        alert('Erro ao carregar a página. Tente novamente mais tarde.');
+      });
+  });
+  
